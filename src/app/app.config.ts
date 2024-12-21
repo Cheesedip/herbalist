c658655293e49df0cb8d61bb8b5bbf6af6aa6341
Maikel <maikel@riskchallenger.nl>
2024-12-21 15:57:35 +0100

    remove api keys from git

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,22 +4,15 @@ import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyC9Nkb9IY_Xw4FrtYc0n9gIS-oFK837zpo',
-  authDomain: 'dndprofessions.firebaseapp.com',
-  projectId: 'dndprofessions',
-  storageBucket: 'dndprofessions.firebasestorage.app',
-  messagingSenderId: '1025477845666',
-  appId: '1:1025477845666:web:7d50019f85426d7800adfb',
-};
-
 import { routes } from './app.routes';
+
+import { environment } from '../environment/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
   ],
 };
