781034be265737bc60cdaabbac85b7f59ba7d642
Maikel <maikel@riskchallenger.nl>
2024-12-22 08:58:14 +0100

    add some styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,13 @@ import { PlantPageComponent } from './pages/plant-page/plant-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SignUpPageComponent } from './pages/login/sign-up-page.component';
 import { LoginPageComponent } from './pages/login/login-page.component';
-import { AuthGuard } from '../guards/auth.guard';
 
 export const routes: Routes = [
   // App routes
-  { path: '', component: HomePageComponent, canActivate: [AuthGuard] },
+  { path: '', component: HomePageComponent },
   {
     path: 'plants/:plantId',
     component: PlantPageComponent,
-    canActivate: [AuthGuard],
   },
 
   // Login routes
