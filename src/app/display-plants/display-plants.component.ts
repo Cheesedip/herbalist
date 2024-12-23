0ebb4e44681d855fd6b2eafbd6ded68777814a20
Maikel <maikel@riskchallenger.nl>
2024-12-23 09:26:16 +0100

    add images for rarity

--- src/app/display-plants/display-plants.component.ts
+++ src/app/display-plants/display-plants.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { Plant } from '../../data/plant/plant';
 import { RouterModule } from '@angular/router';
-import { PlantRarityColor } from '../../data/plant/rarity';
+import { PlantRarity, PlantRarityColor } from '../../data/plant/rarity';
 
 @Component({
   selector: 'app-display-plants',
@@ -13,4 +13,9 @@ import { PlantRarityColor } from '../../data/plant/rarity';
 export class DisplayPlantsComponent {
   public plants = input.required<Plant[]>();
   protected PlantRarityColor = PlantRarityColor;
+
+  protected getRarityImgUrl(rarity: PlantRarity): string {
+    console.log({ rarity });
+    return `/assets/rarity/${rarity}.jpg`;
+  }
 }
