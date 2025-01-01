6d8dd3111af19f7de8eaa0ad5268017ac31f8f0d
Maikel <maikel@riskchallenger.nl>
2025-01-01 09:41:43 +0100

    allow to run application lcoally

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,15 +6,23 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 import { routes } from './app.routes';
 
-import { environment } from '../environment/environment';
+import { environment } from '../environment/environment.production';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-    provideFirestore(() => getFirestore()),
-    provideAuth(() => getAuth()),
-  ],
-};
+export const appConfig: ApplicationConfig =
+  environment.environment === 'production'
+    ? {
+        providers: [
+          provideZoneChangeDetection({ eventCoalescing: true }),
+          provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+          provideFirestore(() => getFirestore()),
+          provideAuth(() => getAuth()),
+          provideRouter(routes),
+        ],
+      }
+    : {
+        providers: [
+          provideZoneChangeDetection({ eventCoalescing: true }),
+          provideRouter(routes),
+        ],
+      };
